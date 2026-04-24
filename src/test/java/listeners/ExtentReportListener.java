6444531c7d04b687a64574fd618633bc354e27f4
Namya2479503 <Namya.Singh@cognizant.com>
2026-04-24 16:56:41 +0530

    report add

--- src/test/java/listeners/ExtentReportListener.java
+++ src/test/java/listeners/ExtentReportListener.java
@@ -5,13 +5,11 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-import org.example.FileReadAndWrite.ScreenShotsCode;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class ExtentReportListener implements ITestListener {
-
 
     private static ExtentSparkReporter sparkReporter;
     private static ExtentReports extent;
@@ -19,8 +17,6 @@ public class ExtentReportListener implements ITestListener {
 
     @Override
     public void onStart(ITestContext context) {
-
-
         String reportPath = System.getProperty("user.dir")
                 + "\\ExtentReports\\TestReport.html";
         sparkReporter = new ExtentSparkReporter(reportPath);
@@ -28,10 +24,6 @@ public class ExtentReportListener implements ITestListener {
         sparkReporter.config().setReportName("Search & Filter Test Results");
         sparkReporter.config().setTheme(Theme.DARK);
         sparkReporter.config().setEncoding("UTF-8");
-
-
-        extent = new ExtentReports();
-        extent.attachReporter(sparkReporter);
 
         extent = new ExtentReports();
         extent.attachReporter(sparkReporter);
@@ -53,7 +45,6 @@ public class ExtentReportListener implements ITestListener {
                 ? extent.createTest(methodName, description)
                 : extent.createTest(methodName);
 
-        // Store in ThreadLocal so onTestSuccess / onTestFailure can reach it
         extentTest.set(test);
         extentTest.get().log(Status.INFO, "Test Started : " + methodName);
     }
@@ -70,30 +61,13 @@ public class ExtentReportListener implements ITestListener {
 
         extentTest.get().log(Status.FAIL, "Test Failed : " + methodName);
 
-        extentTest.get().log(Status.FAIL,
-                "Reason : " + result.getThrowable().getMessage());
+        if (result.getThrowable() != null) {
+            extentTest.get().log(Status.FAIL,
+                    "Reason : " + result.getThrowable().getMessage());
+            extentTest.get().fail(result.getThrowable());
+        }
+    }
 
-
-        extentTest.get().fail(result.getThrowable());
-
-//        try {
-//            String screenshotName = "FAIL_" + methodName + "_"
-//                    + System.currentTimeMillis() + ".png";
-//
-//            String screenshotFullPath =
-//                    ""
-//                            + screenshotName;
-//
-//            ScreenShotsCode.takeScreenshots(screenshotName);
-//
-//            extentTest.get().addScreenCaptureFromPath(
-//                    screenshotFullPath, "Failure Screenshot");
-//
-//        } catch (Exception e) {
-//            extentTest.get().log(Status.WARNING,
-//                    "Screenshot could not be captured : " + e.getMessage());
-//        }
-//    }
     @Override
     public void onTestSkipped(ITestResult result) {
         extentTest.get().log(Status.SKIP,
@@ -104,7 +78,6 @@ public class ExtentReportListener implements ITestListener {
                     "Skip Reason : " + result.getThrowable().getMessage());
         }
     }
-
 
     @Override
     public void onFinish(ITestContext context) {
